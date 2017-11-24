2c784f9bb2f91a63fda08cd677b787bc89f86129
gabriel <777canaan@gmail.com>
2017-11-24 21:12:42 +1300

    4 tests passing keys added and "pollution" issue adresssed

--- present/src/components/App.test.js
+++ present/src/components/App.test.js
@@ -16,3 +16,8 @@ it('adds a new gift to `state` when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click')
   expect(app.state().gifts).toEqual([{ id: 1 }])
 })
+
+it('add a new gift to the rendered list when clicking `add gift` button', () => {
+  app.find('.btn-add').simulate('click')
+  expect(app.find('.gift-list').children().length).toEqual(2)
+})
