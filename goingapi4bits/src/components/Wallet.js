9fc0d2323c26dc4f78fc07acc74b4c5ab5767536
gabriel <777canaan@gmail.com>
2017-11-29 21:05:36 +1300

    input field tested and working

--- goingapi4bits/src/components/Wallet.js
+++ goingapi4bits/src/components/Wallet.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class Wallet extends Component {
+  constructor() {
+    super()
+      this.State = { balance: undefined }
+  }
+
+updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10)
+})
+
   render() {
     return (
       <div>
         <h3 className='balance'>How much in your skyrocket!: {this.props.balance}</h3>
+        <br />
+        <input className='input-wallet' onChange={this.updateBalance} />
       </div>
     )
   }
--- goingapi4bits/src/components/wallet.test.js
+++ goingapi4bits/src/components/wallet.test.js
@@ -11,4 +11,18 @@ describe('Wallet', () => {
   it('displays the balance from the props', () => {
     expect(wallet.find('.balance').text()).toEqual('How much in your skyrocket!: 20')
   })
+  it('creates an input to deposit or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true)
+  })
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '23'
+    beforeEach(() => {
+      wallet.find('.input-wallet').simulate('change', {
+        target: { value: userBalance }
+      })
+    })
+    it('updates the local wallet balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
+    })
+  })
 })
