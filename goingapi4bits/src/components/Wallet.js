57f9aa57f336bd398bf6e1443793216d9aa72f05
gabriel <777canaan@gmail.com>
2017-11-29 22:38:33 +1300

    cookies added balance is persisting

--- goingapi4bits/src/components/Wallet.js
+++ goingapi4bits/src/components/Wallet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { deposit } from '../actions/balance'
+import { deposit, withdraw } from '../actions/balance'
+
 
 export class Wallet extends Component {
   constructor() {
@@ -12,16 +13,18 @@ updateBalance = event => this.setState({ balance: parseInt(event.target.value, 1
 })
 
 deposit = () => this.props.deposit(this.state.balance)
+withdraw = () => this.props.withdraw(this.state.balance)
 
   render() {
     return (
       <div>
-        <h3 className='balance'>How much in your skyrocket!: {this.props.balance}</h3>
+        <h3 className='balance'>How much in your skyrocket! : {this.props.balance}</h3>
         <br />
         <input className='input-wallet' onChange={this.updateBalance} />
         <button className='btn-deposit' onClick={this.deposit}>Deposit</button>
+        <button className='btn-withdraw' onClick={this.withdraw}>Withdraw</button>
       </div>
     )
   }
 }
-export default connect(state => { return {balance: state} }, { deposit })(Wallet)
+export default connect(state => { return {balance: state} }, { deposit, withdraw })(Wallet)
--- goingapi4bits/src/components/wallet.test.js
+++ goingapi4bits/src/components/wallet.test.js
@@ -4,13 +4,14 @@ import {Wallet} from './Wallet'
 
 describe('Wallet', () => {
   const mockDeposit = jest.fn()
-  const props = { balance: 20, deposit: mockDeposit }
+  const mockWithdraw = jest.fn()
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw }
   const wallet = shallow(<Wallet {...props}/>)
   it('renders properly', () => {
     expect(wallet).toMatchSnapshot()
   })
   it('displays the balance from the props', () => {
-    expect(wallet.find('.balance').text()).toEqual('How much in your skyrocket!: 20')
+    expect(wallet.find('.balance').text()).toEqual('How much in your skyrocket! : 20')
   })
   it('creates an input to deposit or withdraw from the balance', () => {
     expect(wallet.find('.input-wallet').exists()).toBe(true)
@@ -26,9 +27,15 @@ describe('Wallet', () => {
       expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
     })
     describe('and the user wants to make a deposit', () => {
-      beforeEach(() => wallet.find('.btn-deposit').simulate('click'  ))
-      it('dispatches the `deposit()` it receives from props with local balance', () => {
+      beforeEach(() => wallet.find('.btn-deposit').simulate('click'))
+      it('dispatches the `deposit()` it receives from props with the local balance', () => {
         expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10))
+      })
+    })
+    describe('and the user wants to make a withdrawal', () => {
+      beforeEach(() => wallet.find('.btn-withdraw').simulate('click'))
+      it('dispatches the `withdraw()` it recieves from props with the local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10))
       })
     })
   })
